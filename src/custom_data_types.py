11e37741110e5ae30cf38b7889330dacb629b9ab
mdparkes <pr3@KM3H2THGK9.local>
2024-02-21 14:54:29 -0700

    Initial commit

---
+++ src/custom_data_types.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+
+from torch import Tensor
+from typing import Any, Dict, List, Set, Tuple, Union
+
+# Custom types for type hints
+NodeInfoDict = Dict[str, Dict[str, Any]]
+Numeric = Union[float, int, bool]
+AdjacencyDict = Dict[str, Union[Set[str], Set[Tuple[str, Tuple[str]]]]]
+ArrayLike = Union[List, np.ndarray, Tensor]
+NumericArrayLike = Union[List[Numeric], Tuple[Numeric], np.ndarray, pd.Series, pd.DataFrame, Tensor]
