42d22bebbdb801c0fa4ab0611afe711522305724
mdparkes <pr3@KM3H2THGK9.local>
2024-02-21 17:04:52 -0700

    Cleaned up script

--- src/get_gdc_data.R
+++ src/get_gdc_data.R
@@ -18,31 +18,10 @@ get_drug_data <- function(cancer) {
 subset_drug_data <- function(x) {
   x[x==""] <- NA
   sel_rows <- is.na(x$drug_name) | is.na(x$measure_of_response)
-  # sel_cols <- c(
-  #   "project",
-  #   "bcr_patient_barcode",
-  #   "therapy_types",
-  #   "drug_name",
-  #   "measure_of_response",
-  #   "days_to_drug_therapy_start",
-  #   "days_to_drug_therapy_end"
-  # )
   x[!sel_rows, ]
 }
 
 # Get GDC clinical data ####
-# tcga_types <- c(
-#   "BLCA",
-#   "BRCA",
-#   "COAD",
-#   "GBM",
-#   "LIHC",
-#   "LUAD",
-#   "LUSC",
-#   "STAD",
-#   "UCEC"
-# )
-
 tcga_types <- c(
   "ACC",
   "BLCA",
